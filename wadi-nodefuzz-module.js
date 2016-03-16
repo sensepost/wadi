be950a65b511ce9e9a607e0048d09c4395f18614
Saif El Sherei <saif@elsherei.com>
2016-03-16 14:23:44 +0000

    Update wadi-nodefuzz-module.js

--- wadi-nodefuzz-module.js
+++ wadi-nodefuzz-module.js
@@ -2501,7 +2501,7 @@ NodeIteratorInterface = {
 	'Attributes':[
 		['Node',[function() {return rint(2)  ? "document.createElement('"+ra(ElementArray)[0].tagName+"')":ra([returnRandomElement(),ra(createdText).objName,'createdElements[\''+ra(createdText).objName+'\']'])}],'readonly'],
 		['whatToShow',[randoms,"1","2","3","0xFFFFFFFF","0x00000001","0x00000002","0x00000004","0x00000008","0x00000010","0x00000020","0x00000040","0x00000080","0x00000100","0x00000200","0x00000400","0x00000800"],'readonly'],
-		['filter',[function() {return "{acceptNode:"+ra(['NodeFilter.FILTER_REJECT','NodeFilter.FILTER_SKIP','NodeFilter.FILTER_REJECT',randoms()])+"}"}],'readonly'],
+		['filter',[function() {return "{acceptNode:"+ra(['NodeFilter.FILTER_ACCEPT','NodeFilter.FILTER_REJECT','NodeFilter.FILTER_SKIP','NodeFilter.FILTER_REJECT',randoms()])+"}"}],'readonly'],
 		['expandEntityReferences',[randbool,randoms],'readonly']
 	],
 	'Methods':[
@@ -2517,7 +2517,7 @@ TreeWalkerInterface = {
 	'Attributes':[
 		['Node',[function() {return rint(2)  ? "document.createElement('"+ra(ElementArray)[0].tagName+"')":ra([returnRandomElement(),ra(createdText).objName,'createdElements[\''+ra(createdText).objName+'\']'])}],'readonly'],
 		['whatToShow',[randoms,"1","2","3","0xFFFFFFFF","0x00000001","0x00000002","0x00000004","0x00000008","0x00000010","0x00000020","0x00000040","0x00000080","0x00000100","0x00000200","0x00000400","0x00000800"],'readonly'],
-		['filter',[function() {return "{acceptNode:"+ra(['NodeFilter.FILTER_REJECT','NodeFilter.FILTER_SKIP','NodeFilter.FILTER_REJECT',randoms()])+"}"}],'readonly'],
+		['filter',[function() {return "{acceptNode:"+ra(['NodeFilter.FILTER_ACCEPT','NodeFilter.FILTER_REJECT','NodeFilter.FILTER_SKIP','NodeFilter.FILTER_REJECT',randoms()])+"}"}],'readonly'],
 		['expandEntityReferences',[randbool,randoms],'readonly'],
 		['currentNode',[function() {return rint(2)  ? "document.createElement('"+ra(ElementArray)[0].tagName+"')":ra([returnRandomElement(),ra(createdText).objName,'createdElements[\''+ra(createdText).objName+'\']'])}],'high']
 	],
